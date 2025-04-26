23ae2a36bfba9f63ae69b54292b081d033a330b6
Abel <abelgetahun66@gmail.com>
2025-04-26 10:02:45 +0300

    Add generated link providers for managing links and filtered links using Riverpod.

---
+++ lib/providers/link_providers.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'link_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$linksNotifierHash() => r'f526c1846a1486c9a82bdf6f677922dd7783becb';
+
+/// See also [linksNotifier].
+@ProviderFor(linksNotifier)
+final linksNotifierProvider = AutoDisposeProvider<LinksNotifier>.internal(
+  linksNotifier,
+  name: r'linksNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$linksNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef LinksNotifierRef = AutoDisposeProviderRef<LinksNotifier>;
+String _$filteredLinksHash() => r'b54126ab0353c760ef2a08779d4851eb9b864d38';
+
+/// See also [filteredLinks].
+@ProviderFor(filteredLinks)
+final filteredLinksProvider = AutoDisposeProvider<List<Link>>.internal(
+  filteredLinks,
+  name: r'filteredLinksProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$filteredLinksHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FilteredLinksRef = AutoDisposeProviderRef<List<Link>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
